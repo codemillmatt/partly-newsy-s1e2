e81a9b43fe112f3e058fbedeeabcc44365f02bda
Matt Soucoup <msoucoup@codemilltech.com>
2019-08-13 13:49:41 -0700

    Final

---
+++ PartlyNewsy.Core/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PartlyNewsy.Core;
+using PartlyNewsy.Models;
+using Xamarin.Forms;
+
+namespace PartlyNewsy
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();            
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            await GetData();
+        }
+
+        async Task GetData()
+        {
+            var svc = new NewsService();
+
+            var articles = await svc.GetTopNews();
+
+            newsList.ItemsSource = articles;
+        }
+    }
+}
---
+++ PartlyNewsy.Core/Services/NewsService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PartlyNewsy.Models;
+using Refit;
+
+namespace PartlyNewsy.Core
+{
+    public class NewsService
+    {
+        readonly string newsFunctionUrl = "http://localhost:7071/api";
+        readonly INewsFunctionAPI newsFunctionAPI;
+
+        public NewsService()
+        {
+            newsFunctionAPI = RestService.For<INewsFunctionAPI>(newsFunctionUrl);
+        }
+
+        public async Task<List<Article>> GetTopNews()
+        {
+            return await newsFunctionAPI.GetTopNewsFromFunction();
+        }
+    }
+
+    public interface INewsFunctionAPI
+    {
+        [Get("/GetTopNews")]
+        Task<List<Article>> GetTopNewsFromFunction();
+    }
+}
