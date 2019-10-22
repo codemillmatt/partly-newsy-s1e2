54c22d8783db9e5e8b538ee7c8b4bea3d96c2c09
Matt Soucoup <msoucoup@codemilltech.com>
2019-10-22 16:01:13 -0700

    Updating NuGets, Droid local netword

--- PartlyNewsy.Core/Services/NewsService.cs
+++ PartlyNewsy.Core/Services/NewsService.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using PartlyNewsy.Models;
 using Refit;
+using Xamarin.Essentials;
 
 namespace PartlyNewsy.Core
 {
     public class NewsService
     {
-        readonly string newsFunctionUrl = "http://localhost:7071/api";
+        readonly string newsFunctionUrl = "http://192.168.86.209:7071/api";
         readonly INewsFunctionAPI newsFunctionAPI;
 
         public NewsService()
         {
+            // this is to account for Android emulator pecularities in local networking
+            // https://developer.android.com/studio/run/emulator-networking.html
+            if (DeviceInfo.DeviceType == DeviceType.Virtual && DeviceInfo.Platform == DevicePlatform.Android)
+                newsFunctionUrl = "http://10.0.2.2:7071/api";
+
             newsFunctionAPI = RestService.For<INewsFunctionAPI>(newsFunctionUrl);
         }
 
